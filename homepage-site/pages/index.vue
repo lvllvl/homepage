5bd9caa7a07092a30a9b9d4dd5074f4d773eb96c
lvllvl <chrisidxloop@gmail.com>
2023-11-18 18:10:34 -0500

    add more content to landing page

--- homepage-site/pages/index.vue
+++ homepage-site/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <h1>null-space.tv</h1>
+    <p>This is my minimalist website. More content to come.</p>
+    <!-- Add more content or links here -->
+  </div>
 </template>
 
 <script>
@@ -7,3 +11,46 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style>
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 1.2em;
+  line-height: 1.6;
+}
+</style>
+
+<style>
+body {
+  font-family: 'Roboto Mono', monospace; /* Correct comment syntax */
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+  font-family: 'Roboto Mono', monospace;
+}
+
+h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 1.2em;
+  line-height: 1.6;
+}
+</style>
